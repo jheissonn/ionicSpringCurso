57580a48c62751e0e432e30cafe843416c832422
jheisson.santos <jh.eison@hotmail.com>
2020-02-26 22:53:16 -0300

    Listando categorias

---
+++ src/services/domain/categoria.service.ts
@@ -0,0 +1,21 @@
+import { CategoriaDTO } from './../../models/categoria.dto';
+import { API_CONFIG } from './../../config/api.config';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs/Rx';
+
+@Injectable()
+export class CategoriaService {
+    id: String;
+    nome: String;
+
+    constructor(public http: HttpClient){
+
+    }
+
+    findAll(): Observable<CategoriaDTO[]>{
+        return this.http.get<CategoriaDTO[]>(`${API_CONFIG.baseUrl}/categorias`);
+    }
+
+
+}
