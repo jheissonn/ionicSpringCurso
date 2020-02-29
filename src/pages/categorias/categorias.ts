c69e84d088b393da83cf6fee397632b72d52c49d
jheisson.santos <jh.eison@hotmail.com>
2020-02-29 12:15:29 -0300

    Listando categorias com imagens

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,3 +1,5 @@
+import { API_CONFIG } from './../../config/api.config';
+import { CategoriaDTO } from './../../models/categoria.dto';
 import { CategoriaService } from './../../services/domain/categoria.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -16,7 +18,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
-  constructor(
+  items: CategoriaDTO[];
+  bucketUrl: String =API_CONFIG.bucketBaseUrl;
+  constructor( 
     public navCtrl: NavController, 
     public navParams: NavParams,
     public categoriaService: CategoriaService) {
@@ -25,7 +29,7 @@ export class CategoriasPage {
   ionViewDidLoad() {
     this.categoriaService.findAll()
     .subscribe(response =>{
-      console.log(response);
+      this.items = response;
     },
     error => {
       console.log(error);
