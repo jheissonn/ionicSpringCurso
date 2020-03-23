8396ca77051ab18e8a19f4ede5e154ee688fd6b4
jheisson.santos <jh.eison@hotmail.com>
2020-03-22 22:11:30 -0300

    Terminando funcionalidade do carrinho

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,3 +1,4 @@
+import { ProdutoDTO } from './../../models/produto.dto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CartItem } from '../../models/cart-item';
@@ -36,5 +37,25 @@ export class CartPage {
         },
         error => {});
     }
+  }
+  
+  removeItem(produto: ProdutoDTO) {
+    this.items = this.cartService.removeProduto(produto).items;
+  }
+
+  increaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.increaseQuantity(produto).items;
+  }
+
+  decreaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.decreaseQuantity(produto).items;
+  }
+
+  total() : number {
+    return this.cartService.total();
   }  
+
+  goOn() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
 }
