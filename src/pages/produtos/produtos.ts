345410cb4f9379c6c5049361bb651d289d323966
jheisson.santos <jh.eison@hotmail.com>
2020-03-25 21:46:07 -0300

    Componente loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,7 +1,7 @@
 import { API_CONFIG } from './../../config/api.config';
 import { ProdutoService } from './../../services/domain/produto.service';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 
 @IonicPage()
@@ -16,17 +16,22 @@ export class ProdutosPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public produtoService: ProdutoService) {
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get('categoria_id');
+    let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
         this.loadImageUrls();
+        loader.dismiss();
       },
-      error => {});
+      error => {
+        loader.dismiss();
+      });
     }
 
     loadImageUrls() {
@@ -42,5 +47,13 @@ export class ProdutosPage {
 
     showDetail(produto_id : string) {
     this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde..."
+    });
+    loader.present();
+    return loader;
   }
 }
