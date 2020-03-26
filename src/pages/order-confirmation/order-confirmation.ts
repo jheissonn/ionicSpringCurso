7d2de40d5b7d1a72ca16b1f2172d857a9d748b10
jheisson.santos <jh.eison@hotmail.com>
2020-03-25 21:40:30 -0300

    Informando pedido registro ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -55,12 +56,15 @@ export class OrderConfirmationPage {
   back() {
     this.navCtrl.setRoot('CartPage');
   }
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
   
   checkout() {
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
+        this.codpedido = this.extractId(response.headers.get('location'));
       },
       error => {
         
@@ -68,5 +72,9 @@ export class OrderConfirmationPage {
           this.navCtrl.setRoot('HomePage');
         }
       });
+  }
+  private extractId(location : string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 }
